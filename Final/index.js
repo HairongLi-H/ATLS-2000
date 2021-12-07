1f4a2cdc6e8c5da460165c24a02ba49ece876f2c
HairongLi-H <89488305+HairongLi-H@users.noreply.github.com>
2021-12-07 14:01:43 -0700

    update

--- Final/index.js
+++ Final/index.js
@@ -24,3 +24,22 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+
+var mybutton = document.getElementById("myBtn");
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
