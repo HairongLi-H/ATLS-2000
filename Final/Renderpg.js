1a55397dc28bbfda320befa1f3c728e81d605770
HairongLi-H <89488305+HairongLi-H@users.noreply.github.com>
2021-12-09 20:58:19 -0700

    update

---
+++ Final/Renderpg.js
@@ -0,0 +1,24 @@
+let pics = 10;
+let pic;
+
+for(i=0;i<pics;i++){
+  pic = 1+i;
+document.getElementById('gallery').innerHTML += "<img src='assets/Render/" + pic + ".jpg'" + "class='gallerypic' id='pic" + pic+ "' />";
+}
+var mybutton = document.getElementById("myBtn");
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
